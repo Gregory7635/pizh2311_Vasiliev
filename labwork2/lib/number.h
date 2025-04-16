0c36ce76e2457252a75b3b9a83937de3c41be755
gregory.vasiliev <greg0ry.vasiliev@yandex.ru>
2025-04-16 17:57:11 +0300

    create number.h

---
+++ labwork2/lib/number.h
@@ -0,0 +1,28 @@
+// labwork2/lib/number.h
+#pragma once
+#include <cinttypes>
+#include <iostream>
+#include <array>
+#include <string>
+
+struct uint2022_t {
+    static const size_t NUM_BITS = 2022;
+    static const size_t NUM_WORDS = (NUM_BITS + 31) / 32;
+    std::array<uint32_t, NUM_WORDS> parts;
+
+    uint2022_t() {
+        parts.fill(0);
+    }
+};
+
+static_assert(sizeof(uint2022_t) <= 300, "Size of uint2022_t must be no higher than 300 bytes");
+
+uint2022_t from_uint(uint32_t i);
+uint2022_t from_string(const char* buff);
+uint2022_t operator+(const uint2022_t& lhs, const uint2022_t& rhs);
+uint2022_t operator-(const uint2022_t& lhs, const uint2022_t& rhs);
+uint2022_t operator*(const uint2022_t& lhs, const uint2022_t& rhs);
+uint2022_t operator/(const uint2022_t& lhs, const uint2022_t& rhs);
+bool operator==(const uint2022_t& lhs, const uint2022_t& rhs);
+bool operator!=(const uint2022_t& lhs, const uint2022_t& rhs);
+std::ostream& operator<<(std::ostream& stream, const uint2022_t& value);
---
+++ labwork2/out/build/x64-Debug/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Gregory/Desktop/labwork2
+# Build directory: C:/Users/Gregory/Desktop/labwork2/out/build/x64-Debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("lib")
+subdirs("bin")
+subdirs("tests")
